d3c7566f3c06eda7bef7b84c5a786474f38cd67b
Aryaman Sharda <aryamansharda@Aryamans-MacBook-Pro.local>
2014-04-11 22:00:59 -0700

    Custom Search Bar

--- LAHacks/ViewController.h
+++ LAHacks/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    IBOutlet UITextField *searchField;
+}
+@property(nonatomic, retain) UIImage *textFieldBackground;
 @end
