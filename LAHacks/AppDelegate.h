889afb4f223bb6d648931e640802694d735a4668
Aryaman Sharda <aryamansharda@Aryamans-MacBook-Pro.local>
2014-04-11 21:44:42 -0700

    Initial Commit

---
+++ LAHacks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LAHacks
+//
+//  Created by Aryaman Sharda on 4/11/14.
+//  Copyright (c) 2014 LaHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LAHacks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LAHacks
+//
+//  Created by Aryaman Sharda on 4/11/14.
+//  Copyright (c) 2014 LaHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
