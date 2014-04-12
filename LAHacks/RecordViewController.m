d37b115c3e829b866805e0611d62c0bc1fa629cc
Aryaman Sharda <aryamansharda@Aryamans-MacBook-Pro.local>
2014-04-12 02:29:05 -0700

    Removed all warnings

--- LAHacks/RecordViewController.m
+++ LAHacks/RecordViewController.m
@@ -169,7 +169,7 @@ bool alreadyStopped = NO;
 
 
 #pragma mark - EZMicrophoneDelegate
-#warning Thread Safety
+
 // Note that any callback that provides streamed audio data (like streaming microphone input) happens on a separate audio thread that should not be blocked. When we feed audio data into any of the UI components we need to explicity create a GCD block on the main thread to properly get the UI to work.
 -(void)microphone:(EZMicrophone *)microphone
  hasAudioReceived:(float **)buffer
