4a04ee5c4183d8c669747f199260db27e4ddbb50
Zaid Elkurdi <zaid.elkurdi@gmail.com>
2014-04-12 02:29:29 -0700

    before pull

---
+++ LAHacks/searchBar.h
@@ -0,0 +1,13 @@
+//
+//  searchBar.h
+//  LAHacks
+//
+//  Created by Zaid Elkurdi on 4/12/14.
+//  Copyright (c) 2014 LaHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface searchBar : UIView
+
+@end
---
+++ LAHacks/searchBar.m
@@ -0,0 +1,31 @@
+//
+//  searchBar.m
+//  LAHacks
+//
+//  Created by Zaid Elkurdi on 4/12/14.
+//  Copyright (c) 2014 LaHacks. All rights reserved.
+//
+
+#import "searchBar.h"
+
+@implementation searchBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
